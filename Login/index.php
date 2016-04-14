f268a921e803e2bc23da93c12f5aa695327a485c
Erik Naess <naess1991@gmail.com>
2016-04-14 16:00:15 +0200

    Version update

--- Login/index.php
+++ Login/index.php
@@ -11,38 +11,62 @@
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Untitled Document</title>
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="../css/form-elements.css">
+        <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-<form name="loginform" action="login_exec.php" method="post">
-<table width="309" border="0" align="center" cellpadding="2" cellspacing="5">
-  <tr>
-    <td colspan="2">
-		<!--the code bellow is used to display the message of the input validation-->
-		 <?php
-			if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
-			echo '<ul class="err">';
-			foreach($_SESSION['ERRMSG_ARR'] as $msg) {
-				echo '<li>',$msg,'</li>'; 
-				}
-			echo '</ul>';
-			unset($_SESSION['ERRMSG_ARR']);
-			}
-		?>
-	</td>
-  </tr>
-  <tr>
-    <td width="116"><div align="right">Username</div></td>
-    <td width="177"><input name="username" type="text" /></td>
-  </tr>
-  <tr>
-    <td><div align="right">Password</div></td>
-    <td><input name="password" type="text" /></td>
-  </tr>
-  <tr>
-    <td><div align="right"></div></td>
-    <td><input name="" type="submit" value="login" /></td>
-  </tr>
-</table>
-</form>
+   <!-- Top content -->
+        <div class="top-content" id = "login">
+
+            <div class="inner-bg">
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 form-box">
+                        	<div class="form-top">
+                        		<div class="form-top-left">
+                        			<h3>Login to our site</h3>
+                            		<p>Enter your username and password to log on:</p>
+                        		</div>
+                        		<div class="form-top-right">
+                        			<i class="fa fa-key"></i>
+                        		</div>
+                            </div>
+                            <div class="form-bottom">
+			                    <form role="form"  method="post" class="login-form" action="login_exec.php">
+			                    <tr>
+                                    <td colspan="2">
+                                		<!--the code bellow is used to display the message of the input validation-->
+                                		 <?php
+                                			if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 ) {
+                                			echo '<ul class="err">';
+                                			foreach($_SESSION['ERRMSG_ARR'] as $msg) {
+                                				echo '<li>',$msg,'</li>';
+                                				}
+                                			echo '</ul>';
+                                			unset($_SESSION['ERRMSG_ARR']);
+                                			}
+                                		?>
+                                	</td>
+                                  </tr>
+			                    	<div class="form-group">
+			                    		<label class="sr-only" for="form-username">Username</label>
+			                        	<input type="text" name="username" placeholder="Username..." class="form-username form-control" id="form-username">
+			                        </div>
+			                        <div class="form-group">
+			                        	<label class="sr-only" for="form-password">Password</label>
+			                        	<input type="password" name="password" placeholder="Password..." class="form-password form-control" id="form-password">
+			                        </div>
+			                        <button type="submit"  class="btn">Sign in!</button>
+			                    </form>
+		                    </div>
+                        </div>
+                    </div>
+<script src="../js/jquery-1.11.1.min.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+
+        <script src="../js/scripts.js"></script>
 </body>
 </html>
