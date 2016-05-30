bab361611416b9bd65a3d4bfcde409dfaeaaf0b2
Mostafa Danaei <mostafa.danaei@gmail.com>
2016-05-30 12:21:28 +0200

    removal sidebar

--- Login/sidebar.php
+++ Login/sidebar.php
@@ -4,21 +4,14 @@
                 <li class="sidebar-brand">
                         <? echo $_SESSION['USERNAME']; ?>
                 </li>
-                <li>
-                    <a href="#">Dashboard</a>
-                </li>
+
                 <li>
                     <a href="add_movie.php">Add movie</a>
                 </li>
                 <li>
                     <a href="edit_movies.php">Edit movie</a>
                 </li>
-                <li>
-                    <a href="#">Events</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
+              
                 <br />
                 <br />
                 <li>
